41f27a4cc564483019ece36e6373294f342deed8
RemigayloYegor <144631320+RemigayloYegor@users.noreply.github.com>
2023-12-11 00:17:25 +0300

    Update 3-1.c

--- 3-1.c
+++ 3-1.c
@@ -9,7 +9,7 @@
  * на то, что он является числом
  * @return введеное значения пользователя
  */
-float scan_f();
+float scan_f(const char* const message);
 
 /**
  * @brief Считает уравнение с проверкой истиности Check и выводит итоговое
@@ -27,16 +27,20 @@ float Example(float x, float d_x, float x_max);
  * @param x_max - максимальное значение в диапазоне
  * @return Если не существует значение х возращает false, иначе true
  */
-bool Check(float x, float d_x, float x_max);
+bool CheckArguments(float x, float d_x, float x_max);
+
+bool CheckFunction(float x);
 
 /**
  * @brief точка входа в программу
  * @return код ошибки (0 - успех)
  */
-int main()
+int main() 
 {
-  float d_x = scan_f(), x = scan_f(), x_max = scan_f();
-  while (x - x_max < DBL_EPSILON) 
+  float d_x = scan_f("Введите значение шага в диапазоне: "), x = scan_f("Введите текущее значение числа в диапазоне: "), 
+  x_max = scan_f("Введите максимальное значение в диапазоне: ");
+  CheckArguments(x, d_x, x_max);
+  while (x - x_max < DBL_EPSILON)
   {
     Example(x, d_x, x_max);
     x = x + d_x;
@@ -46,30 +50,42 @@ int main()
 
 float Example(float x, float d_x, float x_max)
 {
-  if (Check(x, d_x, x_max) == true)
+  if (CheckFunction(x))
   {
     printf("%f\n", 3 * sin(sqrtf(x)) + 0.39 * x - 3.8);
-  } else {
+  }
+  else 
+  {
     printf("%f %s\n", x, "Решение отстутствует");
   }
   return 0;
 }
 
-float scan_f()
+float scan_f(const char* const message) 
 {
   int result;
   float get;
+  printf("%s", message);
   result = scanf("%f", &get);
-  if (result != 1) 
+  if (result != 1)
   {
     abort();
   }
   return get;
 }
 
-bool Check(float x, float d_x, float x_max)
+bool CheckArguments(float x, float d_x, float x_max)
 {
-  if (x < DBL_EPSILON || d_x < DBL_EPSILON || x - x_max > -DBL_EPSILON)
+  if (d_x < DBL_EPSILON || x - x_max > -DBL_EPSILON)
+  {
+    printf("%s", "Error");
+    abort();
+  }
+  return true;
+}
+bool CheckFunction(float x)
+{
+  if (x < DBL_EPSILON)
   {
     return false;
   }
