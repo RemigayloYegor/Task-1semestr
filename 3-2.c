9a46d3daac0caa2f082145a2f317a7d158d9167f
RemigayloYegor <144631320+RemigayloYegor@users.noreply.github.com>
2023-12-20 21:00:18 +0300

    Update 3-2.c

--- 3-2.c
+++ 3-2.c
@@ -1,61 +1,88 @@
-#include <math.h>
 #include <stdio.h>
+#include <float.h>
+#include <errno.h>
 #include <stdlib.h>
+#include <math.h>
 
 /**
- * @brief Функция расчета формулы
- * @param x,y,z значения параметров
- * @return значение вычеслений
- */
-int fun_scan();
+ * @brief Функция присваивает целочисленное значение переменной
+ * @param - сообщение для пользователя
+ * @return целочисленная цифра
+*/
+int scan_f(const char* const message);
+/**
+* @brief Функция расчитывающая сумму членов последовательности.
+* @param length - количество членов последовательности.
+* @return cумма последовательности.
+*/
+double Suma(int length);
+/**
+* @brief Функция расчитывающая сумму членов последовательностиначиная с begin.
+* @param length - количество членов последовательности.
+* @param begin - начальный член последовательности.
+* @return cумма последовательности.
+*/
+double SumaBegin(int begin, int length);
+/**
+* @brief Функция расчитывающая рекурентный член последовательности.
+* @param number - номер члена последовательности.
+* @return Значение рекурентного члена последовательности.
+*/
+double recurent(int number);
 
-float fun_summa(int kol);
-
-float fun_summa_limit(int kol, int limit);
-
-int factorial(int k);
-
-int main() {
-  int quantity, limit;
-  quantity = fun_scan();
-  limit = fun_scan();
-  printf("%f\n", fun_summa(quantity));
-  printf("%f\n", fun_summa_limit(quantity, limit));
+/**
+* @brief Точка входа в программу.
+* @return Возвращает 0 в случае успеха.
+*/
+int main()
+{
+  int length = scan_f("Введите длину последовательности: ");
+  printf("Сумма до %d равна %lf \n", length, Suma(length));
+  int begin = scan_f("Введите число от которого начнем считать: ");
+  printf("Сумма с %d до %d = %lf \n", begin, length, SumaBegin(begin, length));
   return 0;
 }
 
-float fun_summa(int quantity) {
-  float summa;
-  for (int i = 1; i <= quantity; i++) {
-    summa +=
-        pow(-1, factorial(i)) * (i / (pow(factorial(i), 2) + factorial(i + 1)));
+int scan_f(const char* const message)
+{
+  int length;
+  printf("%s", message);
+  int result = scanf("%d", &length);
+  if (result != 1 || length < 1)
+  {
+    errno = EIO;
+    perror("Error :");
+    abort();
   }
-  return summa;
+  return length;
 }
 
-float fun_summa_limit(int quantity, int limit) {
-  float summa;
-  for (int i = limit; i <= quantity; i++) {
-    summa +=
-        pow(-1, factorial(i)) * (i / (pow(factorial(i), 2) + factorial(i + 1)));
+double Suma(int length)
+{
+  double current = 1;
+  double result = 0;
+  for(int k = 0; k < length; k++)
+  {
+      current *= recurent(k);
+      result += current;
   }
-  return summa;
-}
-
-int factorial(int k) {
-  int result = 1;
-  for (int i = 1; i <= k; i++)
-    result = result * i;
   return result;
 }
 
-int fun_scan() {
-  int a;
-  int b;
-  a = scanf("%d", &b);
-  if (a != 1) {
-    abort();
-  } else {
-    return b;
+double SumaBegin(int begin, int length)
+{
+  double current = 1;
+  double result = 0;
+  int k = begin;
+  while(length - k >= -DBL_EPSILON)
+  {
+      current *= recurent(k);
+      result += current;
+      k++;
   }
+  return result;
+}
+double recurent(int number)
+{
+  return 1.0 / (4 * pow(number, 2) + 6 * number + 2);
 }
