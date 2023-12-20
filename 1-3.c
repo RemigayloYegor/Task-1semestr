99fd4ab88acf4951e42a517d9b62bae8f89ce079
RemigayloYegor <144631320+RemigayloYegor@users.noreply.github.com>
2023-12-20 17:59:10 +0300

    Update 1-3.c

--- 1-3.c
+++ 1-3.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <float.h>
 /**
  * @brief функция написана, чтобы пользователи вводили значение и проверять его
  * на то, что он является числом
@@ -11,10 +13,10 @@ float scan_f();
 /**
  * @brief Проверяет существования силы тока 2-ой лампы
  * @param I - общая сила тока
- * @param I1 -  сила тока первой лампы
+ * @param I1 - сила тока первой лампы
  * @return I2 силу тока второй лампы
  */
-float fun_check(float I, float I1);
+float Check(float I, float I1);
 
 /**
  * @brief считает сопротивление лампы
@@ -27,15 +29,13 @@ float resistance(float I2);
  * @brief точка входа в программу
  * @return код ошибки (0 - успех)
  */
-int main()
-{
+int main() {
   float I1, I2, I, R2;
   I = scan_f();
   I1 = scan_f();
-  I2 = fun_check(I, I1);
+  I2 = Check(I, I1);
   R2 = resistance(I2);
-  printf("%s %f\n %s %f", "Сила тока второй лампы =", I2,
-         "Сопротивление второй лампы =", R2);
+  printf("%s %f\n %s %f", "Сила тока второй лампы =", I2, "Сопротивление второй лампы =", R2);
 }
 
 float resistance(float I2) {
@@ -49,18 +49,19 @@ float scan_f() {
   result = scanf("%f", &get);
   if (result != 1)
   {
+    errno = EIO;
+    perror("Error :");
     abort();
   }
   return get;
 }
 
-float fun_check(float I, float I1) {
+float Check(float I, float I1) {
   float I2;
   I2 = I - I1;
-  if (I2 < 0)
+  if (I2 < DBL_EPSILON)
   {
     abort();
   } 
   return I2;
 }
-
