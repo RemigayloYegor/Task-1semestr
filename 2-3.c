32ec311a0622c1a512a81f79a82c98ac41cc7b22
RemigayloYegor <144631320+RemigayloYegor@users.noreply.github.com>
2023-12-20 18:31:59 +0300

    Update 2-3.c

--- 2-3.c
+++ 2-3.c
@@ -1,49 +1,77 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 /**
- * @brief функция написана, чтобы пользователи вводили значение и проверять его
- * на то, что он является числом
- * @return введеное значения пользователя
- */
-float fun_scan();
-
+ * @brief Функция присваивает целочисленное значение переменной
+ * @param - сообщение для пользователя
+ * @return целочисленная цифра
+*/
+int scan_f(const char* const message);
 /**
- * @brief Функция выбирает вариант перевести угол из радианов в градусы или из
- * градусов в радины
- * @param  corner - угол, varible - вариант перевода
- * @return угол в градусах или радианах
+ * @brief Функция переводит угол из радианов в градусы 
+ * @param  corner - угол
+ * @return угол в градусах
  */
-float fun_variable(int variable, float corner);
-
-int main() {
-  float number;
-  int variable;
-  printf("%s\n", "Если хотите перевести из радианов в градусы введите 1, если "
-                 "из градусов в радины то 2.");
-  variable = fun_scan();
-  printf("%s\n", "Введите колличество");
-  number = fun_scan();
-  fun_variable(variable, number);
-}
-
-float fun_variable(int variable, float corner) {
-  if (variable == 1) {
-    return printf("%f", corner * 180 / M_PI);
-  } else if (variable == 2) {
-    return printf("%f", corner * M_PI / 180);
-  } else {
-    abort();
+float Radians_Degrees(float corner);
+/**
+ * @brief Функция переводит угол из градусов в радины
+ * @param  corner - угол
+ * @return угол в радианах
+ */
+float Degrees_Radians(float corner);
+/**
+ * @brief структура хранит константы, указывающие выбор пользователя заполнить массив
+ * @brief RadiansToDegrees хранит значение, вызывающее функцию перевода радиан в градусы
+ * @brief DegreesToRadians хранит значение, вызывающее функцию перевода градусов в радианы
+*/
+enum Choices
+{
+    RadiansToDegrees = 1,
+    DegreesToRadians = 2
+};
+/**
+ * @brief точка входа в программу
+ * @return код ошибки (0 - успех)
+ */
+int main()
+{
+  const int choice = scan_f("Если хотите перевести из радианов в градусы введите 1, если из градусов в радины то 2.\n");
+  float corner = scan_f("Введите число: ");
+  switch ((enum Choices)choice)
+  {
+      case RadiansToDegrees:
+              printf("%f", Radians_Degrees(corner));
+          break;
+      case DegreesToRadians:
+              printf("%f", Degrees_Radians(corner));
+          break;
+      default:
+          puts("Insert a valid choice!\n");
+          return 1;
   }
 }
 
-float fun_scan() {
-  int result;
-  float get;
-  result = scanf("%f", &get);
-  if (result != 1) {
-    abort();
-  }
-  return get;
+float Radians_Degrees(float corner)
+{
+  return corner * 180 / M_PI;
 }
+float Degrees_Radians(float corner)
+{
+  return corner * M_PI / 180;
+}
+int scan_f(const char* const message)
+{
+    int value = 0;
+    printf("%s", message);
+    int result = scanf("%d", &value);
+    if (result != 1)
+    {
+        errno = EIO;
+        perror("Error :");
+        abort();
+    }
+    return value;
+}
+
