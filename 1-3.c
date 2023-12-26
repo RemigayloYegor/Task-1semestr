1cd779a9ec6864bdb951619cdce4053cffea5025
RemigayloYegor <144631320+RemigayloYegor@users.noreply.github.com>
2023-12-26 10:38:17 +0300

    Update 1-3.c

--- 1-3.c
+++ 1-3.c
@@ -28,24 +28,18 @@ float resistance(float I2);
  * @brief точка входа в программу
  * @return код ошибки (0 - успех)
  */
-int main()
-{
-  float I1, I2, I, R2;
-  I = scan_f();
-  I1 = scan_f();
-  I2 = Check(I, I1);
-  R2 = resistance(I2);
+int main() {
+  float I1 = scan_f(), I = scan_f(), 
+  I2 = Check(I, I1), R2 = resistance(I2);
   printf("%s %f\n %s %f", "Сила тока второй лампы =", I2, "Сопротивление второй лампы =", R2);
 }
 
-float resistance(float I2) 
-{
+float resistance(float I2) {
   float const U = 220;
   return U / I2;
 }
 
-float scan_f()
-{
+float scan_f() {
   int result;
   float get;
   result = scanf("%f", &get);
@@ -58,11 +52,10 @@ float scan_f()
   return get;
 }
 
-float Check(float I, float I1)
-{
+float Check(float I, float I1) {
   float I2;
   I2 = I - I1;
-  if (I2 < DBL_EPSILON)
+  if (I2 * I1 > -DBL_EPSILON)
   {
     abort();
   } 
